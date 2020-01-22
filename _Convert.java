e1ece5ae7309ca9570ad3a5a02ac68bdb313149f
amir mohammad davoodi <34346335+amirdavoodi98@users.noreply.github.com>
2020-01-22 13:54:20 -0800

    Add files via upload

---
+++ _Convert.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.*;
+
+public class _Convert {
+
+    public static String boolToInt(String str)
+    {
+        if(str.equals("true"))
+            return "1";
+        else
+            return "0";
+    }
+    public static String intToString(String str)
+    {
+        if(str.equals("0"))
+            return "\\X00";
+        else
+            return "\\X01";
+    }
+    public static String intToBool(String str)
+    {
+        if(str.equals("0"))
+            return "false";
+        else
+            return "true";
+    }
+    public static String intToFloat(String str)
+    {
+        return String.valueOf(Float.parseFloat(str));
+    }
+   /* public static String userDifinedToInt(String str)
+    {
+
+    }*/
+
+}
